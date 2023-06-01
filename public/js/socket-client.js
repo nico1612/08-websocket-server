668e182f74712c5da245b1cccfe9d8a3fa839b4e
Alejandro Gonzalez <alejandrogonzalez@AirdeAlejandro-3.fibertel.com.ar>
2023-06-01 18:33:43 -0300

    cambios

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,2 +1,19 @@
 
+const lblOnline=document.querySelector('#lbl-online')
+const lblOffline=document.querySelector('#lbl-offline')
+
 const socket=io();
+
+socket.on('connect',()=>{
+    console.log('Conectado')
+
+    lblOffline.style.display='none'
+    lblOnline.style.display=''
+}) 
+
+socket.on('disconnect',()=>{
+    console.log('Desconectado')
+
+    lblOnline.style.display='none'
+    lblOnline.style.display=''
+}) 
